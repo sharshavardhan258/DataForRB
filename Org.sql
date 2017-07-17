4d9845bb7894f6ed3c1bac31a3ee0cdf344efa8d
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:21:18 +0530

    Create Org.sql

---
+++ Org.sql
@@ -0,0 +1,21 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Org]
+           ([First Name]
+           ,[Last Name]
+           ,[Email]           
+           ,[Password]
+           ,[Contact Number]          
+           ,[Gender]          
+           ,[Address line 1]       
+           ,[City]
+           ,[State]
+           ,[Zip Code]
+           ,[Active]
+           ,[Mode]
+           ,[Type]
+           ,[LoginType])
+     VALUES
+    		   ('Harsha','Vardhan','sharshavardhan258@gmail.com','OTQ5MzIzNjY2NQ==',9493236665,'Male','Denver Broncos opp Uc health','Denver','Colorado',400031,1,'Register','Organization','linkedin')
+GO
