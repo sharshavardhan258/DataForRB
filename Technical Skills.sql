866b5878d5d835f7fc4175049597b122ff3bd661
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:23:07 +0530

    Create Technical Skills.sql

---
+++ Technical Skills.sql
@@ -0,0 +1,16 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Technical Skills]
+           ([Technical Skill Name])
+     VALUES
+           (<Technical Skill Name, varchar(max),>)
+
+		   ('Windows Administration'),
+		   ('Architecture'),
+		   ('Storage'),
+		   ('Clustering'),
+		   ('Windows Server'),
+		   ('Scheduling')
+GO
+
