61d70d726a5d34572cc6f9a1879396205e40c7bb
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:16:18 +0530

    Create Certification.sql

---
+++ Certification.sql
@@ -0,0 +1,14 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Certification]
+           ([From]
+           ,[To]
+           ,[CourseName]
+           ,[Institution]
+           ,[Location]
+           ,[WorkforceId])
+     VALUES
+           
+		   (2014-02-12,2014-05-20,'.Net','Naresh Institute',300590)
+GO
