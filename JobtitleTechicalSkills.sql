0a4a99562bba71db19967eeac3495b8ed99bd84a
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:20:47 +0530

    Create JobtitleTechicalSkills.sql

---
+++ JobtitleTechicalSkills.sql
@@ -0,0 +1,13 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[JobtitleTechicalSkills]
+           ([JobTitleID]
+           ,[TechnicalSkillID])
+     VALUES
+           (<JobTitleID, int,>
+           ,<TechnicalSkillID, int,>)
+		   (104000,106001),
+		   (104000,106002),
+		   (104001,106004)
+GO
