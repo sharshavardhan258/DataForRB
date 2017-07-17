a3d7aedbff50f85a15c46e54f0f57d4bec7c4fe2
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:25:31 +0530

    Create Vendors.sql

---
+++ Vendors.sql
@@ -0,0 +1,26 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Vendors]
+           ([Name]
+           ,[Address 1]
+           ,[City]
+           ,[State]
+           ,[Country]
+           ,[Zip]
+           ,[Website]
+           ,[Tags]
+           ,[Phone]         
+           ,[Email]
+           ,[Active]
+           ,[OrganizationID]
+           ,[Mode]
+           ,[Type]
+           ,[Password]
+           ,[Role]
+           ,[Rating]
+           )
+     VALUES
+          
+		   ('Ensar Solutions','Medical Drive','Frankfort','Kentucky','USA',42066,'www.ensarsolutions.com',700001,+1(630)465-5666,'uhsekhar@gmail.com',1,103123,'Invitation','Vendor','c2VraGFyMTIz',4)
+GO
