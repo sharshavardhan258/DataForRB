f1e388c07aa25f48557ed88de031d1e1af0957d6
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:18:20 +0530

    Create Education.sql

---
+++ Education.sql
@@ -0,0 +1,13 @@
+USE [RedBenchDB]
+GO
+INSERT INTO [dbo].[Education]
+           ([From]
+           ,[To]
+           ,[CourseName]
+           ,[University]
+           ,[Location]
+           ,[WorkforceId])
+     VALUES
+     
+		   (2006-04-16,2010-06-26,'B.Tech','University of California','Los Angeles',300595)
+GO
