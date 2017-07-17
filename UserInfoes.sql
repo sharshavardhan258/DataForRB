076702fb4ea5ded5923423ffcb29219def965e04
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:24:03 +0530

    Create UserInfoes.sql

---
+++ UserInfoes.sql
@@ -0,0 +1,16 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[UserInfoes]
+           ([Type]
+           ,[Email]
+           ,[Password]
+           ,[LastName]
+           ,[Mode]
+           ,[FirstName]
+           ,[Active]
+           ,[PhoneNumber])
+     VALUES
+
+		   ('Business','bhakta@ensarsolutions.com','U3VtYW50aEAxMA==','ch','Register','sumanth',1,+(140)567-5493)
+GO
