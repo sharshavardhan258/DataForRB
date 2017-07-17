75e1d95da8a8baec0a83792b7065fc5eafe2e27b
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:16:56 +0530

    Create Company.sql

---
+++ Company.sql
@@ -0,0 +1,21 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Company]
+           ([Name]
+           ,[Website]
+           ,[Industry]
+           ,[Size]
+           ,[Revenue]
+           ,[Headquarters]
+           ,[Image ]
+           ,[Address1]
+           ,[Address2]
+           ,[City]
+           ,[State]
+           ,[ZipCode]
+           ,[OrganizationID])
+     VALUES
+         
+		   ('TreeGo Systems','www.treegosystems.com','software',1-100,1000000,'KPHB,7 Phase','Hyderabad','AP',500072,103173)
+GO
