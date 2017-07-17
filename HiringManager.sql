df50fd76eb158591d1c7ad68cbec933b1470e632
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:24:54 +0530

    Create HiringManager.sql

---
+++ HiringManager.sql
@@ -0,0 +1,28 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([First Name]
+           ,[Last Name]
+           ,[Email]
+           ,[Alternate Email]
+           ,[Password]
+           ,[Contact Number]
+           ,[Alternate Contact No]
+           ,[Gender]
+           ,[Image]
+           ,[Address line 1]
+           ,[Address line 2]
+           ,[City]
+           ,[State]
+           ,[Zip Code]
+           ,[Active ]
+           ,[OrganizationID]
+           ,[Mode]
+           ,[Type]
+          )
+     VALUES
+           
+		   ('gary','kirsten','garyk@gmail.com','OTQ5MzIzNjY2NQ=',+(114)493-2365,'Male','108 West College Street','Topeka','Kansas',350521,1,103173,'Invitation','User')
+GO
+
