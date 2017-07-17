e56137def3daab796ed86943c2f0e3f8c5e7fdce
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:21:47 +0530

    Create Resume.sql

---
+++ Resume.sql
@@ -0,0 +1,12 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Resume]
+           ([Path]
+           ,[ResumeName]
+           ,[WorkforceId])
+     VALUES
+           
+		   ('http://redbenchservice.azurewebsites.net/Content/harsha.docx','harsha.docx',300580),
+		   ('http://redbenchservice.azurewebsites.net/Content/resume.docx','resume.docx',300596)
+GO
