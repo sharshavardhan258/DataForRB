cadc0b6bcef5b4d794202a7462f30c32d58ea153
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:18:53 +0530

    Create Experience.sql

---
+++ Experience.sql
@@ -0,0 +1,15 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Experience]
+           ([From]
+           ,[To]
+           ,[Position]
+           ,[Company]
+           ,[Location]
+           ,[WorkforceId])
+     VALUES
+    
+		   (2016-05-11,2017-09-18,'Java Manager','Delite','Chicago',300565)
+GO
+
