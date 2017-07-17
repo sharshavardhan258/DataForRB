b630951dba4283639cc84282165237612c83b30b
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:17:45 +0530

    Create ContactandAddress.sql

---
+++ ContactandAddress.sql
@@ -0,0 +1,14 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[ContactandAddress]
+           ([ContactName]
+           ,[JobPosition]
+           ,[Email]
+           ,[Notes]
+           ,[Phone]
+           ,[VendorID])
+     VALUES
+          
+		   ('Sumanth','Software Developer','sumanthc@delitesoft.com','He is working on Angular2',8500838431,200127)
+GO
