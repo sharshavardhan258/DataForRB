ef078904499d8b23a0cab586e681c35a794b6618
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:20:11 +0530

    Create JobTitle.sql

---
+++ JobTitle.sql
@@ -0,0 +1,13 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[JobTitle]
+           ([Job title name])
+     VALUES
+           (<Job title name, varchar(50),>)
+		   ('Windows Manager'),
+		   ('Senior Developer'),
+		   ('java developer'),
+		   ('Angular Developer'),
+		   ('HTML&CSS Developer')
+GO
