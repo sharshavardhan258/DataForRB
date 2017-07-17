6f780caadbbc23026e7bcff60826bde9b84a552a
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-17 20:15:30 +0530

    Create Applications.sql

---
+++ Applications.sql
@@ -0,0 +1,17 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Applications]
+           ([WorkForce]
+           ,[JobPositions]
+           ,[Organization]
+           ,[Vendor]
+           ,[ApplicationStatus]
+           ,[AppliedOn]
+           ,[HiredDate]
+           ,[RejectDate])
+     VALUES
+         
+		 (300505,300058,103119,200130,'Reject',2017-07-05,2017-07-17,2017-07-17)
+
+GO
